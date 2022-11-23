33aa8df22d5ce8529d9dbed0e2a9e12b6b9030db
ivandjuka77 <captainsubzer@gmail.com>
2022-11-23 14:27:59 +0100

    Added pricing component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import About from './components/About/About';
 import Header from './components/Header/Header';
+import Pricing from './components/Pricing/Pricing';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<About />
+			<Pricing />
 		</>
 	);
 }
