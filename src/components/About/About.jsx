6fff48e7f9368c31121fadfd15353f0debef8b53
ivandjuka77 <captainsubzer@gmail.com>
2022-11-23 10:33:54 +0100

    Added about page.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import './About.css';
+
+const About = () => {
+	return (
+		<div className='about'>
+			<div className='about-text'>
+				<h1>Who are we?</h1>
+				<p>
+					Barber Cuts was founded in 2021 by the Abbott family and only
+					consisted of 2 members. <br />
+					Nowdays, our team consists of 10 artists, all with their own style,
+					personality and special touches.
+					<br /> We're a team of passionate barbers who live for the art of
+					cutting hair and making you look better. <br />
+					We take responsibility of you and your well-being while you're in our
+					house, <br />
+					and we love to see you enjoy your stay! <br />
+					<br />
+				</p>
+			</div>
+			<div className='about-gallery'>
+				<img
+					src={require('../../assets/about5.jpg')}
+					alt='barber'
+					className='about-img'
+				/>
+				<img
+					src={require('../../assets/about2.jpg')}
+					alt='barber'
+					className='about-img'
+				/>
+				<img
+					src={require('../../assets/about6.jpg')}
+					alt='barber'
+					className='about-img'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default About;
