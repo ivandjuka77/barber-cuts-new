6098ebd3d3983da1b3a2189a0879c31414e94fc4
ivandjuka77 <captainsubzer@gmail.com>
2022-11-24 13:26:06 +0100

    Dinamically display options.

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,73 +1,111 @@
 import './Booking.css';
+import { useState } from 'react';
 
 const Booking = () => {
-	return (
-		<div className='booking'>
-			<h1>Booking</h1>
-			<form>
+	const [service, setService] = useState('');
+
+	const showSelect = () => {
+		if (service === 'hair') {
+			return (
 				<select
 					name='hair'
 					id='hair'
 				>
-					<option value=''>Hair</option>
-
 					<option value='25'>Men's Haircut 25€</option>
 					<option value='30'>Men's Haircut (Long hair) 30€</option>
 					<option value='20'>Men's Haircut (Juniors) 20€</option>
 					<option value='20'>Shaving 20€</option>
 				</select>
+			);
+		} else if (service === 'beard') {
+			return (
 				<select
 					name='Beard'
 					id='Beard'
 				>
-					<option value=''>Beard</option>
-
 					<option value='20'>Beard Adjustment 20€</option>
 					<option value='25'>Beard Adjustment (Long beard) 25€</option>
 					<option value='20'>Beard Shaving 20€</option>
 				</select>
+			);
+		} else if (service === 'hair-beard') {
+			return (
 				<select
 					name='hair-beard'
 					id='hair-beard'
 				>
-					<option value=''>Hair & Beard</option>
-
 					<option value='35'>Haircut & Beard Adjustment 35€</option>
 					<option value='40'>Haircut & Beard Adjustment (Long) 40€</option>
 					<option value='30'>Beard & Hair Shaving 30€</option>
 				</select>
+			);
+		} else if (service === 'minor-adjustments') {
+			return (
 				<select
 					name='minor-adjustments'
 					id='minor-adjustments'
 				>
-					<option>Minor Adjustments</option>
-
 					<option value='5'>Nose Hair Removal 5€</option>
 					<option value='5'>Ear Cleaning 5€</option>
 					<option value='5'>Eyebrow adjustment 5€</option>
 				</select>
+			);
+		} else if (service === 'coloring') {
+			return (
 				<select
 					name='coloring'
 					id='coloring'
 				>
-					<option>Coloring</option>
 					<option value='10'>Beard Coloring 10€</option>
 					<option value='0'>Hair Coloring (price on arrival)</option>
 				</select>
+			);
+		} else if (service === 'skin') {
+			return (
 				<select
 					name='skin'
 					id='skin'
 				>
-					<option>Skin</option>
 					<option value='10'>Charcoal Peel Off Mask 10€</option>
 				</select>
+			);
+		} else if (service === 'gold') {
+			return (
 				<select
 					name='gold'
 					id='gold'
 				>
-					<option>Gold Combo</option>
 					<option value='70'>Gold Combo 70€</option>
 				</select>
+			);
+		}
+	};
+	return (
+		<div
+			className='booking'
+			id='booking'
+		>
+			<h1>Booking</h1>
+			<form>
+				<label htmlFor='hair'>Choose your service:</label>
+				<select
+					name='service'
+					id='service'
+					onChange={(e) => setService(e.target.value)}
+				>
+					<option
+						value=''
+						defaultValue
+					></option>
+					<option value='hair'>Hair</option>
+					<option value='beard'>Beard</option>
+					<option value='hair-beard'>Hair & Beard</option>
+					<option value='minor-adjustments'>Minor Adjustments</option>
+					<option value='coloring'>Coloring</option>
+					<option value='skin'>Skin</option>
+					<option value='gold'>Gold Combo</option>
+				</select>
+				{showSelect()}
 			</form>
 		</div>
 	);
